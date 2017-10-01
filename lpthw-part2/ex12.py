9289b57e2cb1c3b28d3522d07f530a2b0edd130c
jupiterhub <jupiter.adverts@gmail.com>
2017-10-01 18:51:41 +0800

    added sample of input() within a print

---
+++ lpthw-part2/ex12.py
@@ -0,0 +1,9 @@
+# rewrite inputs form ex11
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"so you're {age} old, {height} tall and {weight} heavy.")
+
+#print("This will ask for input before printing", input())
