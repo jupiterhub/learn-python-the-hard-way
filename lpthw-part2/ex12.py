d3254e668c4c29285b5094a664ed3d215406719d
jupiterhub <jupiter.adverts@gmail.com>
2017-10-01 18:52:41 +0800

    ex12, input within {} does not pause

--- lpthw-part2/ex12.py
+++ lpthw-part2/ex12.py
@@ -6,4 +6,7 @@ weight = input("How much do you weigh? ")
 
 print(f"so you're {age} old, {height} tall and {weight} heavy.")
 
-#print("This will ask for input before printing", input())
+# print("This will ask for input before printing", input())
+
+# this doesnt ask for input
+# print("This will ask for input before printing {input()}")
