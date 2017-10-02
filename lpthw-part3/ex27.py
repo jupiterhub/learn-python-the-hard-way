2f45b79891d8add2a09b4f9966b09691c769e208
jupiterhub <jupiter.adverts@gmail.com>
2017-10-02 17:55:36 +0800

    ex27, no code, just a truth table

---
+++ lpthw-part3/ex27.py
@@ -0,0 +1,10 @@
+# It's just an example of a truth table
+# Side Note: Easiest way to do ML is the truth table
+# 1 | 1 = True
+# 1 | 0 = True
+# 0 | 1 = True
+# 0 | 0 = False
+# 1 & 1 = True
+# 1 & 0 = False
+# 0 & 1 = False
+# 0 & 0 = False
