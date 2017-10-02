7270b925e229ea3d8ede8426c7ba875e15497f3e
jupiterhub <jupiter.adverts@gmail.com>
2017-10-02 20:36:53 +0800

    ex34, accessing elements

---
+++ lpthw-part4/ex34.py
@@ -0,0 +1,9 @@
+# Accessing list elements
+
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo',
+    'whale', 'platypus']
+
+# index 0 is called cardinal numbers (because you can get in random)
+# index 1 is called ordinal (this is how humans use it, ie. imagine a race)
+print(animals[0])
+print(animals[5])
