5e17f96dba3777494128ea61fe121fb107a03636
jupiterhub <jupiter.adverts@gmail.com>
2017-10-02 21:35:53 +0800

    ex36, sample game; like 35; skipping

---
+++ lpthw-part4/ex36.py
@@ -0,0 +1,12 @@
+# Tips
+# for if, always use an else
+# if it doesn't make sense, make it die to easily debug
+# never nest more than 2 if
+# put blank lines before if-elif-else
+# complex conditions should be moved to variable or method
+
+# Write your own game similar to ex35.py
+## Before coding draw a map (look at flow in ex35)
+## list the tasks
+## work on Easiest
+## test your scripts often
