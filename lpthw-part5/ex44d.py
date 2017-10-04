5680c151ccdc855896d9e8c8e5bce9bb33d2c09d
jupiterhub <jupiter.adverts@gmail.com>
2017-10-04 21:11:43 +0800

    Put all the inheritance exapmles together

---
+++ lpthw-part5/ex44d.py
@@ -0,0 +1,34 @@
+# All three inheritance factors cobined
+class Parent(object):
+
+    def override(self):
+        print("PARENT override()")
+
+    def implicit(self):
+        print("PARENT implicit()")
+
+    def altered(self):
+        print("PARENT altered()")
+
+class Child(Parent):
+
+    def override(self):
+        print("CHILD override()")
+
+    def altered(self):
+        print("CHILD, BEFORE PARENT altered()")
+        # super(Child, self).altered() - python2 version
+        super().altered()
+        print("CHILD, AFTER PARENT altered()")
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
+
+dad.altered()
+son.altered()
