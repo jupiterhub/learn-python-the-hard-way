2dfc2f25ef45a2a11d641dab14ae46429fd57eb1
jupiterhub <jupiter.adverts@gmail.com>
2017-10-04 19:08:40 +0800

    implemented the rest of the scenes

--- lpthw-part5/ex43.py
+++ lpthw-part5/ex43.py
@@ -106,12 +106,80 @@ class CentralCorridor(Scene):
 class LaserWeaponArmory(Scene):
 
     def enter(self):
-        pass
+        print(dedent("""
+            You do a dive roll into the Weapon Armory,
+            crouch and scan the room for more Gothons that might be hiding.
+            It's dead quiet, too quiet.
+            You stand up and run to the far side of the room
+            and find the neutron bomb in its container.
+            There's a keypad lock on the box and you need the code to get the bomb out.
+            If you get the code wrong 10 times then the lock closes forever
+            and you can't get the bomb. The code is 3 digits.
+        """))
+
+        # 3 digit code
+        code = f"{randint(1,9)}{randint(1,9)}{randint(1,9)}"
+        guess = input("[keypad]>")
+        guesses = 0
+
+        while guess != code and guesses < 10:
+            print("BZZZZEDDD")
+            guesses += 1
+            guess = input("[keypad]>")
+
+            if guess == code:
+                # go to next scene
+                print(dedent("""
+                    The container clicks open and the seal breaks, letting gas out.
+                    You grab the neutron bomb and run as fast as you can
+                    to the bridge where you must place it in the right spot.s
+                """))
+                return 'the_bridge'
+            else: # this should be outside
+                # try again
+                print(dedent("""
+                    THe lock buzzes on last time and then
+                    you hear a sickening melting sound as the mechanism is fused together.
+                    You decide to sit there, and finally the Gothons blow up the ship
+                    from their ship and you die
+                """))
+                return 'death'
 
 class TheBridge(Scene):
 
     def enter(self):
-        pass
+        print(dedent("""
+            You burst onto the Bridge with the neutron desctruct bomb
+            under your arm and surprise 5 Gothons who are trying to take control of the ship.
+            Each of them has an even uglier clown costume than the last.
+            They haven't pulled their weapons out yet, as they see the active bomb under your arm
+            and don't want to set it off.
+        """))
+
+        action = input("> ")
+
+        if action == "throw the bomb":
+            print(dedent("""
+            In a panic you throw the bomb at the group of Gothons
+            and make a leap for the door.
+            Right as you drop it a Gothon shoots you right in the back killing you.
+            As you die you see another Gothon frantically try to disarm the bomb.
+            You die knowing they will probably blow up when it goes off.
+            """))
+            return 'death'
+        elif action == "slowly place the bomb":
+            print(dedent("""
+                You point your blaster at the bomb under your arm
+                and the Gothons put their hands up and start to sweat.
+                You inch backward to the door, open it, and then
+                carefully place the bomb on the floor, pointing your blaster at it.
+                You then jump back through the door, punch the close button and blast the lock
+                so the Gothons can't get out. Now that the bomb is placed
+                you run to the escape pod to get off this tin can.
+            """))
+        else:
+            print("DOES NOT COMPUTE!")
+            return "the_bridge"
 
 class EscapePod(Scene):
 
