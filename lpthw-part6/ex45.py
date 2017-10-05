2f5f65339863050d37e4f66faafa350ee5bd9af6
jupiterhub <jupiter.adverts@gmail.com>
2017-10-05 20:11:07 +0800

    added overview story

--- lpthw-part6/ex45.py
+++ lpthw-part6/ex45.py
@@ -1,1 +1,19 @@
 # Own game
+# Make a different game# Use more than one file and use import
+# Use Ibe ckass oer riin
+# Runner shuold know about them
+
+# This is worht 2 weeks of work
+# Spend a whole week on this and mak e it the best game you can
+
+# Story - Overview for the game
+# You were working on a mundane job and
+# decided to start a Business.
+# You have a few options, restaurants, technology startup, and a marketing
+# (social media agency)
+# You find people to invest in
+# There are different challenges such as
+# Natural calamties, recessions, cheaper competion overseas
+# You die when your business go bankrupt
+# You win when you're rich, got old, don't have time, and wim.
+# You won ^_^
