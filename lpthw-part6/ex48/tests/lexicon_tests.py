1367fccda1c643bd0da46b9cbeb9b9e7de908a24
jupiterhub <jupiter.adverts@gmail.com>
2017-10-06 23:28:33 +0800

    added a test for parsing verbs

--- lpthw-part6/ex48/tests/lexicon_tests.py
+++ lpthw-part6/ex48/tests/lexicon_tests.py
@@ -7,6 +7,13 @@ def test_directions():
                     ['direction', 'north'])
 
     result = lexicon.scan("north south east")
-    assert_equal(result, ['direction', 'north'],
-                         ['direction', 'south'],
-                         ['direction', 'east'])
+    assert_equal(result, [('direction', 'north')],
+                         [('direction', 'south')],
+                         [('direction', 'east')])
+
+def test_verbs():
+    assert_equal(lexicon.scan("go"), [('verb', 'go')])
+    result  = lexicon.scan("go kill eat")
+    assert_equal(result, [('verb', 'go'),
+                          ('verb', 'kill'),
+                          ('verb', 'eat')])
