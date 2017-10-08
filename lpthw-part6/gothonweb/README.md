6085187e692e13fe9399f4489b1996edc0093ce9
jupiterhub <jupiter.adverts@gmail.com>
2017-10-08 17:26:10 +0800

    Added a template to be loaded when rendering the temlpate

--- lpthw-part6/gothonweb/README.md
+++ lpthw-part6/gothonweb/README.md
@@ -1,5 +1,7 @@
 # lpthw-part6 > gothonweb (ex50)
-a web version of gothon game from ex43.
+a web version of gothon game from ex43 using flask.
+
+Flask has built in debugger, unit test, request dispatch, templating (jinja2), secure cookies, wsgi, unicode
 
 ## Build / install
 ```python setup.py build```
---
+++ lpthw-part6/gothonweb/app.py
@@ -0,0 +1,21 @@
+# ex50.py, simple web server
+# server supports hot reload
+from flask import Flask # web framework/server
+from flask import render_template # loads templates
+# interpreter sets default values for __name__
+# think of Thread name.
+# this is "__main__"
+# if you run the module it will be the module name
+app = Flask(__name__)
+
+# index mappings
+@app.route('/')
+def hello_world():
+    greeting = "Hello World"
+    # looks at templates/ (variables for 2nd argument)
+    return render_template("index.html", greeting=greeting)
+
+# only run the server if
+# running directly (not an import)
+if __name__ == "__main__":
+    app.run()
