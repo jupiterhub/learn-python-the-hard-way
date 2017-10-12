ba32dce4e5273812b9364169766ef05e140ec3a1
jupiterhub <jupiter.adverts@gmail.com>
2017-10-12 16:40:04 +0800

    Added ending scene for wining; pod # is hardcoded

--- lpthw-part6/gothonweb/gothonweb/planisphere.py
+++ lpthw-part6/gothonweb/gothonweb/planisphere.py
@@ -88,3 +88,13 @@ class Room(object):
         but you don't have time to look.
         There's 5 pods, which on do you take?
         """)
+
+        the_end_winner = Room("The End",
+        # todo: pod # to be dynamic
+        """
+        You jump into pod 2 and hit the eject button.
+        The pod easily slides out into space heading to the planet below.
+        As it flies to the planet, you look back and see your ship implode then
+        explode like a bright star, taking out the Gothon ship at the same time.
+        You won!
+        """)
